e49fa02964b1cd65f177aecd3184bdc2e530573d
integralfx <jordansu83@hotmail.com>
2019-06-22 15:10:32 +1000

    Add files via upload

---
+++ SPDEditor.java
@@ -0,0 +1,337 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SPDEditor {
+    private byte[] bytes;
+    // 0x06: 1.25v, 1.35v, not 1.50v
+    private LinkedHashMap<String, Boolean> voltages;
+    private byte tCKmin;            // 0x0C
+    // 0x0E: 11 to 4, 0x0F: 18 to 12
+    private LinkedHashMap<Integer, Boolean> supportedCLs;
+
+    private byte tCLmin;            // 0x10
+    private byte tWRmin;            // 0x11
+    private byte tRCDmin;           // 0x12
+    private byte tRRDmin;           // 0x13
+    private byte tRPmin;            // 0x14
+    private short tRASmin;          // (0x15 bits[3:0]) | (0x16 bits[7:0])
+    private short tRCmin;           // (0x15 bits[7:4]) | (0x17 bits[7:0]) 
+    private short tRFCmin;          // (0x19 bits[7:0]) | (0x18 bits[7:0])
+    private byte tWTRmin;           // 0x1A
+    private byte tRTPmin;           // 0x1B
+    private short tFAWmin;          // (0x1C bits[3:0]) | (0x1D bits[7:0])
+
+    // all in picoseconds (1 thousandth of a ns)
+    private byte tCKminCorrection;  // 0x22
+    private byte tCLminCorrection;  // 0x23
+    private byte tRCDminCorrection; // 0x24
+    private byte tRPminCorrection;  // 0x25
+    private byte tRCminCorrection;  // 0x26
+
+    public SPDEditor(byte[] bytes) throws IllegalArgumentException {
+        if (bytes.length < 128) {
+            throw new IllegalArgumentException("Expected 128 bytes. Got " + 
+                                               bytes.length + " bytes.");
+        }
+
+        this.bytes = Arrays.copyOf(bytes, bytes.length);
+
+        voltages = new LinkedHashMap<>();
+        voltages.put("1.25v", (((bytes[0x06] >> 2) & 0x1) == 0x1));
+        voltages.put("1.35v", (((bytes[0x06] >> 1) & 0x1) == 0x1));
+        voltages.put("1.50v", ((bytes[0x06]) & 0x1) != 0x1); // not 1.50v
+
+        tCKmin = bytes[0x0C];
+
+        supportedCLs = new LinkedHashMap<>();
+        for (int i = 4; i <= 11; i++) {
+            int index = i - 4;
+            supportedCLs.put(i, ((bytes[0x0E] >> index) & 0x1) == 0x1);
+        }
+        for (int i = 12; i <= 18; i++) {
+            int index = i - 12;
+            supportedCLs.put(i, ((bytes[0x0F] >> index) & 0x1) == 0x1);
+        }
+
+        tCLmin = bytes[0x10];
+        tWRmin = bytes[0x11];
+        tRCDmin = bytes[0x12];
+        tRRDmin = bytes[0x13];
+        tRPmin = bytes[0x14];
+        int msb = bytes[0x15] & 0xF;
+        tRASmin = (short)((msb << 8) | Byte.toUnsignedInt(bytes[0x16]));
+        msb = (bytes[0x15] >> 4) & 0xF;
+        tRCmin = (short)((msb << 8) | Byte.toUnsignedInt(bytes[0x17]));
+        tRFCmin = (short)((bytes[0x19] << 8) | Byte.toUnsignedInt(bytes[0x18]));
+        tWTRmin = bytes[0x1A];
+        tRTPmin = bytes[0x1B];
+        msb = bytes[0x1C] & 0xF;
+        tFAWmin = (short)((msb << 8) | Byte.toUnsignedInt(bytes[0x1D]));
+
+        tCKminCorrection = bytes[0x22];
+        tCLminCorrection = bytes[0x23];
+        tRCDminCorrection = bytes[0x24];
+        tRPminCorrection = bytes[0x25];
+        tRCminCorrection = bytes[0x26];
+    }
+
+    public double getFrequency() { 
+        double tCKns = tCKmin/8.0 + tCKminCorrection/1000.0;
+        tCKns = getMorePrecisetCKns(tCKns);
+        return 1000/tCKns; 
+    }
+
+    public void setFrequency(double freq) {
+        double tCKns = 1000/freq;
+        int value = (int)(tCKns*8);
+        double correction = Math.round((1000 * (tCKns*8 - value)/8));
+
+        tCKmin = (byte)value;
+        tCKminCorrection = (byte)correction;
+    }
+
+    public LinkedHashMap<Integer, Boolean> getSupportedCLs() {
+        return supportedCLs;
+    }
+
+    public boolean setSupportedCL(int cl, boolean supported) {
+        if (!supportedCLs.containsKey(cl)) return false;
+
+        supportedCLs.put(cl, supported);
+        return true;
+    }
+
+    public LinkedHashMap<String, Integer> getTimings() {
+        LinkedHashMap<String, Integer> timings = new LinkedHashMap<>();
+
+        double tCKns = tCKmin/8.0 + tCKminCorrection/1000.0;
+        tCKns = getMorePrecisetCKns(tCKns);
+
+        double tCLns = tCLmin/8.0 + tCLminCorrection/1000.0,
+               tCL = Math.round(tCLns/tCKns),
+               tRCDns = tRCDmin/8.0 + tRCDminCorrection/1000.0,
+               tRCD = Math.round(tRCDns/tCKns),
+               tRPns = tRPmin/8.0 + tRPminCorrection/1000.0,
+               tRP = Math.round(tRPns/tCKns),
+               tRASns = Integer.toUnsignedLong(tRASmin)/8.0,
+               tRAS = Math.round(tRASns/tCKns),
+               tRCns = Integer.toUnsignedLong(tRCmin)/8.0 + 
+                       tRCminCorrection/1000.0,
+               tRC = Math.round(tRCns/tCKns),
+               tRFCns = Integer.toUnsignedLong(tRFCmin)/8.0,
+               tRFC = Math.round(tRFCns/tCKns),
+               tRRDns = tRRDmin/8.0,
+               tRRD = Math.round(tRRDns/tCKns),
+               tFAWns = Integer.toUnsignedLong(tFAWmin)/8.0,
+               tFAW = Math.round(tFAWns/tCKns),
+               tWRns = tWRmin/8.0,
+               tWR = Math.round(tWRns/tCKns),
+               tWTRns = tWTRmin/8.0,
+               tWTR = Math.round(tWTRns/tCKns),
+               tRTPns = tRTPmin/8.0,
+               tRTP = Math.round(tRTPns/tCKns);
+
+        timings.put("tCL", (int)tCL);
+        timings.put("tRCD", (int)tRCD);
+        timings.put("tRP", (int)tRP);
+        timings.put("tRAS", (int)tRAS);
+        timings.put("tRC", (int)tRC);
+        timings.put("tRFC", (int)tRFC);
+        timings.put("tRRD", (int)tRRD);
+        timings.put("tFAW", (int)tFAW);
+        timings.put("tWR", (int)tWR);
+        timings.put("tWTR", (int)tWTR);
+        timings.put("tRTP", (int)tRTP);
+
+        return timings;
+    }
+
+    // call setFrequency() before calling setTiming()
+    public boolean setTiming(String timing, int ticks) {
+        if (ticks < 1) return false;
+
+        double tCKns = tCKmin/8.0 + tCKminCorrection/1000.0;
+        tCKns = getMorePrecisetCKns(tCKns);
+
+        double ns = ticks*tCKns;
+        int value = (int)(ns*8);
+        double correction = Math.round((1000 * (ns*8 - value)/8));
+
+        if (timing.equals("tCL")) {
+            if (!supportedCLs.containsKey(ticks)) return false;
+            supportedCLs.put(value, true);
+
+            tCLmin = (byte)value;
+            tCLminCorrection = (byte)correction;
+        }
+        else if (timing.equals("tRCD")) {
+            tRCDmin = (byte)value;
+            tRCDminCorrection = (byte)correction;
+        }
+        else if (timing.equals("tRP")) {
+            tRPmin = (byte)value;
+            tRPminCorrection = (byte)correction;
+        }
+        else if (timing.equals("tRAS")) {
+            tRASmin = (short)value;
+        }
+        else if (timing.equals("tRC")) {
+            tRCmin = (short)value;
+            tRCminCorrection = (byte)correction;
+        }
+        else if (timing.equals("tRFC")) {
+            tRFCmin = (short)value;
+        }
+        else if (timing.equals("tRRD")) {
+            tRRDmin = (byte)value;
+        }
+        else if (timing.equals("tFAW")) {
+            tFAWmin = (short)value;
+        }
+        else if (timing.equals("tWR")) {
+            tWRmin = (byte)value;
+        }
+        else if (timing.equals("tWTR")) {
+            tWTRmin = (byte)value;
+        }
+        else if (timing.equals("tRTP")) {
+            tRTPmin = (byte)value;
+        }
+        else return false;
+
+        return true;
+    }
+
+    public int setTimings(LinkedHashMap<String, Integer> timings) {
+        int count = 0;
+        for (Map.Entry<String, Integer> e : timings.entrySet()) {
+            if (setTiming(e.getKey(), e.getValue()))
+                count++;
+        }
+
+        return count;
+    }
+
+    public void printTimings() {
+        for (Map.Entry<String, Integer> e : getTimings().entrySet()) {
+            System.out.println(e.getKey() + ": " + e.getValue());
+        }
+    }
+
+    public boolean save(String filename) {
+        updateBytes();
+
+        short crc = crc16XModem();
+        System.out.printf("CRC: %X", crc);
+        // little endian - LSB goes last
+        bytes[0x7F] = (byte)((crc >> 8) & 0xFF);
+        bytes[0x7E] = (byte)(crc & 0xFF);
+
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
+            fos.write(bytes);
+            return true;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private double getMorePrecisetCKns(double tCKns) {
+        int value = (int)Math.round(1000*tCKns);
+        // 1066.666...
+        if (value == 938) return 0.9375;
+        // 933.333...
+        else if (value == 1071) return 1000 / (2800/3.0);
+        else return tCKns;
+    }
+
+    // source: http://mdfs.net/Info/Comp/Comms/CRC16.htm
+    private short crc16XModem() {
+        int crc = 0;
+        for (int i = 0; i <= 0x74; i++) {
+            crc ^= bytes[i] << 8;
+
+            for (int j = 0; j < 8; j++) {
+                crc <<= 1;
+                if ((crc & 0x10000) == 0x10000)
+                    crc = (crc ^ 0x1021) & 0xFFFF;
+            }
+        }
+
+        return (short)crc;
+    }
+
+    private void updateBytes() {
+        int n = 0;
+        if (voltages.get("1.25v")) n |= (1 << 2);
+        if (voltages.get("1.35v")) n |= (1 << 1);
+        if (!voltages.get("1.50v")) n |= 1;
+        bytes[0x06] = (byte)n;
+
+        bytes[0x0C] = tCKmin;
+
+        n = 0;
+        for (int i = 4; i <= 11; i++) {
+            int index = i - 4;
+            if (supportedCLs.get(i)) n |= (1 << index);
+        }
+        bytes[0x0E] = (byte)n;
+        n = 0;
+        for (int i = 12; i <= 18; i++) {
+            int index = i - 12;
+            if (supportedCLs.get(i)) n |= (1 << index);
+        }
+        bytes[0x0F] = (byte)n;
+
+        bytes[0x10] = tCLmin;
+        bytes[0x11] = tWRmin;
+        bytes[0x12] = tRCDmin;
+        bytes[0x13] = tRRDmin;
+        bytes[0x14] = tRPmin;
+        int msb = (tRASmin >> 8) & 0xF;
+        bytes[0x15] = (byte)msb;
+        bytes[0x16] = (byte)(tRASmin & 0xFF);
+        msb = (tRCmin >> 8) & 0xFF;
+        bytes[0x15] |= (byte)(msb << 4);
+        bytes[0x17] = (byte)(tRCmin & 0xFF);
+        bytes[0x18] = (byte)(tRFCmin & 0xFF);
+        bytes[0x19] = (byte)((tRFCmin >> 8) & 0xF);
+        bytes[0x1A] = tWTRmin;
+        bytes[0x1B] = tRTPmin;
+        bytes[0x1C] = (byte)((tFAWmin >> 8) & 0xF);
+        bytes[0x1D] = (byte)(tFAWmin & 0xFF);
+
+        bytes[0x22] = tCKminCorrection;
+        bytes[0x23] = tCLminCorrection;
+        bytes[0x24] = tRCDminCorrection;
+        bytes[0x25] = tRPminCorrection;
+        bytes[0x26] = tRCminCorrection;
+    }
+
+    public static void main(String[] args) {
+        /*
+        try {
+            File f = new File("c-die_2133C11-13-12.bin");
+            SPDEditor spd = new SPDEditor(Files.readAllBytes(f.toPath()));
+
+            spd.setFrequency(800);
+            spd.setTiming("tCL", 10);
+            spd.setTiming("tRCD", 10);
+            spd.setTiming("tRP", 10);
+            spd.setTiming("tRAS", 30);
+            spd.setSupportedCL(11, false);
+            spd.save("test.bin");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        */
+    }
+}
---
+++ SPDEditorGUI.java
@@ -0,0 +1,318 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
+
+public class SPDEditorGUI extends JFrame {
+    private SPDEditor spd;
+    private JLabel lblSPDFile;
+    private JTextField txtFrequencyns, txtFrequency;
+    private LinkedHashMap<String, TextFieldPair> nameTextFieldMap;
+    private LinkedHashMap<Integer, JCheckBox> clCheckBoxMap;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new SPDEditorGUI());
+    }
+
+    public SPDEditorGUI() {
+        super("SPD Editor");
+
+        addMenuBar();
+
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        JPanel panel = new JPanel();
+        lblSPDFile = new JLabel("No SPD file opened");
+        panel.add(lblSPDFile);
+        add(panel);
+
+        Frequency[] frequencies = {
+            new Frequency(1200/3.0), // 400
+            new Frequency(1600/3.0), // 533.33...
+            new Frequency(2000/3.0), // 666.66...
+            new Frequency(2400/3.0), // 800
+            new Frequency(2800/3.0), // 933.33...
+            new Frequency(3200/3.0)  // 1066.66...
+        };
+        panel = new JPanel();
+        panel.add(new JLabel("Frequency:"));
+        //JComboBox<Frequency> cboFrequencies = new JComboBox<>(frequencies);
+        //panel.add(cboFrequencies);
+        txtFrequencyns = new JTextField(5);
+        txtFrequencyns.setEditable(false);
+        panel.add(txtFrequencyns);
+        txtFrequency = new JTextField(5);
+        txtFrequency.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean valid = true;
+                String s = txtFrequency.getText();
+                try {
+                    double f = Double.valueOf(s);
+                    if (f < 400) valid = false;
+                    else {
+                        txtFrequencyns.setText(String.format("%.3f", 1000/f));
+                        /*
+                         * Timings are calculated from the cycle time (ns), 
+                         * which don't change when changing frequency. 
+                         * Thus, we have to save the timings, update the
+                         * frequency, then update with the timings that we have
+                         * saved.
+                         */
+                        //LinkedHashMap<String, Integer> t = spd.getTimings();
+                        spd.setFrequency(f);
+                        //spd.setTimings(t);
+                        updateTimingsText();
+                    }
+                }
+                catch (NumberFormatException ex) {
+                    valid = false;
+                }
+
+                if (valid)
+                    txtFrequency.setBackground(Color.WHITE);
+                else
+                    txtFrequency.setBackground(new Color(255, 100, 100));
+            }
+        });
+        panel.add(txtFrequency);
+        add(panel);
+
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        clCheckBoxMap = new LinkedHashMap<>();
+        int cl = 4;
+        for (int row = 0; row < 3; row++) {
+            JPanel p = new JPanel();
+            for (int col = 0; col < 5; col++) {
+                if (cl > 18) break;
+
+                JCheckBox chk = new JCheckBox(String.valueOf(cl));
+                chk.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        spd.setSupportedCL(Integer.valueOf(chk.getText()), 
+                                           chk.isSelected());
+                    }
+                });
+                p.add(chk);
+                clCheckBoxMap.put(cl, chk);
+                cl++;
+            }
+            panel.add(p);
+        }
+        Border b = BorderFactory.createLineBorder(Color.BLACK);
+        panel.setBorder(BorderFactory.createTitledBorder(b, "Supported CLs"));
+        add(panel);
+
+        JPanel timingsPanel = new JPanel(new GridLayout(6, 2, 10, 10));
+        String[] timingNames = {
+            "tCL", "tRCD", "tRP", "tRAS", "tRC","tRFC", 
+            "tRRD", "tFAW", "tWR", "tWTR", "tRTP"
+        };
+        nameTextFieldMap = new LinkedHashMap<>();
+        for (String name : timingNames) {
+            panel = new JPanel();
+
+            JLabel lbl = new JLabel(name + ":");
+            lbl.setFont(lbl.getFont().deriveFont(Font.BOLD));
+            panel.add(lbl);
+
+            JTextField txtns = new JTextField(5);
+            txtns.setEditable(false);
+            panel.add(txtns);
+            JTextField txtTicks = new JTextField(3);
+            txtTicks.addActionListener(new ActionListener(){
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    boolean valid = true;
+                    String s = txtTicks.getText();
+                    try {
+                        int t = Integer.valueOf(s);
+                        if (t < 0) valid = false;
+                        else if (name.equals("tCL")) {
+                            if (t < 4 || t > 18) valid = false;
+                            else {
+                                spd.setTiming(name, t);
+                                if (clCheckBoxMap.containsKey(t))
+                                    clCheckBoxMap.get(t).setSelected(true);
+                            }
+                        }
+                        else spd.setTiming(name, t);
+
+                        updateTimingsText();
+                    }
+                    catch (NumberFormatException ex) {
+                        valid = false;
+                    }
+
+                    if (valid)
+                        txtTicks.setBackground(Color.WHITE);
+                    else
+                        txtTicks.setBackground(new Color(255, 100, 100));
+                }
+            });
+            panel.add(txtTicks);
+            timingsPanel.add(panel);
+
+            nameTextFieldMap.put(name, 
+                                 new TextFieldPair(txtns, txtTicks));
+        }
+        timingsPanel.setBorder(BorderFactory.createTitledBorder(b, "Timings"));
+        add(timingsPanel);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    private void addMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu menuFile = new JMenu("File");
+        menuBar.add(menuFile);
+
+        JMenuItem menuItemOpen = new JMenuItem("Open"),
+                  menuItemSaveAs = new JMenuItem("Save as");
+        menuFile.add(menuItemOpen);
+        menuFile.add(menuItemSaveAs);
+
+        ActionListener l = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fc = new JFileChooser(Paths.get(".").toFile());
+                
+                if (e.getSource() == menuItemOpen) {
+                    if (fc.showOpenDialog(getContentPane()) == 
+                        JFileChooser.APPROVE_OPTION) {
+                        File file = fc.getSelectedFile();
+                        try {
+                            spd = new SPDEditor(
+                                Files.readAllBytes(file.toPath())
+                            );
+
+                            lblSPDFile.setText(file.getName());
+                            updateGUI();
+                        }
+                        catch (Exception ex) {
+                            spd = null;
+                            showErrorMsg("Failed to read " + file.getName());
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+                else if (e.getSource() == menuItemSaveAs) {
+                    if (spd == null) {
+                        showErrorMsg("Please open an SPD file first");
+                        return;
+                    }
+
+                    if (fc.showSaveDialog(getContentPane()) == 
+                        JFileChooser.APPROVE_OPTION) {
+                        String path = fc.getSelectedFile().getAbsolutePath();
+                        if (spd.save(path))
+                            showSuccessMsg("Successfully saved to " + path);
+                        else showErrorMsg("Failed to save SPD file");
+                    }
+                }
+            }
+        };
+        menuItemOpen.addActionListener(l);
+        menuItemSaveAs.addActionListener(l);
+
+        setJMenuBar(menuBar);
+    }
+
+    private void updateGUI() {
+        SwingUtilities.invokeLater(() -> {
+            double f = spd.getFrequency();
+            txtFrequencyns.setText(String.format("%.3f", 1000/f));
+            txtFrequency.setText(String.format("%.2f", f));
+    
+            // update supported CLs
+            LinkedHashMap<Integer, Boolean> cls = spd.getSupportedCLs();
+            for (Map.Entry<Integer, Boolean> e : cls.entrySet()) {
+                if (clCheckBoxMap.containsKey(e.getKey()))
+                    clCheckBoxMap.get(e.getKey()).setSelected(e.getValue());
+            }
+    
+            updateTimingsText();
+        });
+    }
+
+    private void updateTimingsText() {
+        SwingUtilities.invokeLater(() -> {
+            for (Map.Entry<String, Integer> e : spd.getTimings().entrySet()) {
+                if (nameTextFieldMap.containsKey(e.getKey())) {
+                    TextFieldPair pair = nameTextFieldMap.get(e.getKey());
+                    pair.left.setText(
+                        String.format("%.3f", 
+                                      1000/spd.getFrequency()*e.getValue())
+                    );
+                    pair.right.setText("" + e.getValue());
+                }
+            }
+        });
+    }
+
+    private void showErrorMsg(String msg) {
+        JOptionPane.showMessageDialog(
+            this,
+            msg,
+            "Error",
+            JOptionPane.ERROR_MESSAGE
+        );
+    }
+    
+    private void showSuccessMsg(String msg) {
+        JOptionPane.showMessageDialog(
+            this,
+            msg,
+            "Success",
+            JOptionPane.INFORMATION_MESSAGE
+        );
+    }
+}
+
+class TextFieldPair {
+    public final JTextField left, right;
+    public TextFieldPair(JTextField l, JTextField r) {
+        left = l;
+        right = r;
+    }
+}
+
+class Frequency {
+    public final double frequency;
+    public Frequency(double f) {
+        frequency = f;
+    }
+
+    @Override
+    public String toString() { 
+        return String.format("%.2f", frequency); 
+    }
+}
